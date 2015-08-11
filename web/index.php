2ae12595a4220803103b8818b3dbce2a3137ec1c
steffkes <matheis.stefan@gmail.com>
2015-08-11 16:11:36 +0200

    added debug information so we're able to follow changes due to a deployment

--- web/index.php
+++ web/index.php
@@ -1,1 +1,11 @@
-<?php phpinfo();
+<?php
+
+$debug = [
+  'DOCUMENT_ROOT' => $_SERVER['DOCUMENT_ROOT'],
+  '__DIR__' => __DIR__,
+  'REVISION' => trim(file_get_contents(sprintf('%s/../REVISION', __DIR__)))
+];
+
+echo sprintf('<pre>%s</pre>', print_r($debug, true));
+
+phpinfo();
